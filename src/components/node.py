9cf6976801e91ddf6a9cde12ba4b93d27c65677a
Matej Lesko <mlesko@redhat.com>
2017-06-30 14:32:41 +0200

    NEW: ClientNode now receives class isntead of instance
    Desc:
    BasicSenderNode and BasicReceiverNode now initialize new clients
    with every method call of send/receive. Therefore, these methods were
    rewritten.

--- src/components/node.py
+++ src/components/node.py
@@ -1,39 +1,36 @@
+from src.components.client import Sender, Receiver
 from src.tools.receiver import SimpleReceiverHandler
 from src.tools.sender import SimpleSenderHandler
 
 
 class Node(object):
-    def __init__(self, hostname):
+    def __init__(self, hostname="localhost"):
         self.hostname = hostname
 
 
 class ClientNode(Node):
     def __init__(self, hostname, client=None):
         super(ClientNode, self).__init__(hostname)
-        self.client = client
+        self.client_class = client
+        self.client = None
 
     @property
     def last_message(self):
         return self.client.message_pool[-1]
 
 class BasicSenderNode(ClientNode):
-    def __init__(self, hostname, sender=SimpleSenderHandler):
-        super(BasicSenderNode, self).__init__(hostname, sender())
+    def __init__(self, hostname, sender=Sender):
+        super(BasicSenderNode, self).__init__(hostname, sender)
 
-    def send(self, node, address=None, count=1, message=None):
-        self.client.hostname = node.hostname
-        self.client.address = address or self.client.address  # use default value if given is None
-        self.client.count = count
-        self.client.message = message or self.client.message
+    def send(self, node, address=None, count=1, messages=[]):
+        self.client = self.client_class(hostname=node.hostname, address=address, count=count, messages=messages)
         self.client.run()
 
 
 class BasicReceiverNode(ClientNode):
-    def __init__(self, hostname, receiver=SimpleReceiverHandler):
-        super(BasicReceiverNode, self).__init__(hostname, receiver())
+    def __init__(self, hostname, receiver=Receiver):
+        super(BasicReceiverNode, self).__init__(hostname, receiver)
 
-    def receive(self, node, address=None, count=1):
-        self.client.hostname = node.hostname
-        self.client.address = address or self.client.address  # use default value if given is None
-        self.client.count = count
+    def receive(self, node, address=None, expected=1):
+        self.client = self.client_class(hostname=node.hostname, address=address, expected=expected)
         self.client.run()
