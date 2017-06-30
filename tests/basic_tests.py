f7e52686055bfcd419db80c0656b7621ae61bfef
Matej Lesko <mlesko@redhat.com>
2017-06-30 14:41:17 +0200

    UPD: basic_test.py now contain first working test
    Desc: Test comparing message ids of sent and received message

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -1,8 +1,10 @@
 import pytest
 
+from tests import get_func_name
+from proton import Message
 
 def test_send_receive(in_node, out_node, sender_node, receiver_node):
-    # if in_node pytest.skip
-    sender_node.send(in_node)
-    receiver_node.receive(out_node)
-    assert sender_node.last_message == receiver_node.last_message
+    message = Message()
+    sender_node.send(node=in_node, address=get_func_name(), messages=[message])
+    receiver_node.receive(node=out_node, address=get_func_name())
+    assert sender_node.last_message._get_id() == receiver_node.last_message._get_id()
